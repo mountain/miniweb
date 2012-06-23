10ee522daef810cd54708109ad9d8685b2259cbf
Farrin Reid <blakmatrix@gmail.com>
2012-06-23 15:43:10 -0800

    [fix] Changed require('util') to require('util') for compatibility with node v0.8

--- scripts/create.js
+++ scripts/create.js
@@ -1,18 +1,18 @@
-var sys  = require('sys'),
+var util = require('util'),
     fs   = require('fs'),
     path = require('path'),
     fsutil = require('../lib/fsutil');
 
 module.exports = function (proj) {
     if (!proj) {
-        sys.puts('No project name found.');
+        util.puts('No project name found.');
         process.exit(1);
     }
-    sys.puts('create project ' + proj);
+    util.puts('create project ' + proj);
     var p = path.join(process.cwd(), proj);
     path.exists(p, function (exists) {
         if (exists) {
-            sys.puts('error: path ' + p + ' already exists');
+            util.puts('error: path ' + p + ' already exists');
         } else {
             fsutil.mkdir(p);
             fsutil.copydir(path.join(__dirname, '..', 'prototype'), p);
